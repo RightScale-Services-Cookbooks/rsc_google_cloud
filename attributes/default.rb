cb7d55d8d543593d639379d14136ebd22eec282c
Richard Shade <rshade@rightscale.com>
2013-11-12 13:20:10 -0600

    adding metadata required

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
+require '/var/spool/cloud/metadata.rb'
+
 default[:google_cloud][:project]=node[:rsc_google_cloud][:instance_id].split('/')[1]
 default[:google_cloud][:instance_id]=node[:rsc_google_cloud][:instance_id].split('/').last
 default[:google_cloud][:zone_id]=ENV['GCE_ZONE'].split('/').last
