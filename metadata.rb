cf2d2a8993b872556a176bf80a16bc4c3257fddf
Richard Shade <rshade@rightscale.com>
2013-11-12 12:01:12 -0600

    adding first round

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'rsc_google_cloud'
+maintainer       'RightScale Inc'
+maintainer_email 'premium@rightscale.com'
+license          'Apache 2.0'
+description      'Installs/Configures rsc_google_cloud'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
