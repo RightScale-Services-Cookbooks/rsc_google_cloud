6cd7cb2367dfe488617ae2c8493f8ce9ba286982
Richard Shade <rshade@rightscale.com>
2013-11-12 13:31:08 -0600

    fixing the metadata path

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-require '/var/spool/cloud/metadata.rb'
+require '/var/spool/cloud/meta-data.rb'
 
 default[:google_cloud][:project]=node[:rsc_google_cloud][:instance_id].split('/')[1]
 default[:google_cloud][:instance_id]=node[:rsc_google_cloud][:instance_id].split('/').last
