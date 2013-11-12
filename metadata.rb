9628d8d9c3308532bed17a94f7038197c4a585c3
Richard Shade <rshade@rightscale.com>
2013-11-12 16:00:14 -0600

    adding region for lb_attach

--- metadata.rb
+++ metadata.rb
@@ -24,3 +24,15 @@ attribute "google_cloud/lb/pool_name",
   :description => "Google LB Pool Name",
   :required => "required",
   :recipes => [ "rsc_google_cloud::lb_do_attach", "rsc_google_cloud::lb_do_detach" ]
+
+attribute "google_cloud/region",
+  :display_name => "Google Cloud Region",
+  :description => "Google Cloud Region",
+  :choice => [ "us-central1", "us-central2", "europe-west1" ],
+  :required => "required"
+
+attribute "google_cloud/lb/tag",
+  :display_name => "Google LB Firewall Tag",
+  :description => "Google LB Firewall Tag",
+  :required => "optional",
+  :default => "www"
