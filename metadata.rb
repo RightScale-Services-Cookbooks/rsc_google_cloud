8dedaa53c7e98ed940c2c12eb585ce5b6084e7a7
Curt Wilhelm <curt@rightscale.com>
2015-03-13 11:56:56 -0600

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'premium@rightscale.com'
 license          'Apache 2.0'
 description      'Installs/Configures rsc_google_cloud'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '1.2.1'
 
 depends "marker"
 depends "gce"
