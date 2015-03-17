af383c2477157f5b50b1ad4077000e404bf3f831
Curt Wilhelm <curt@rightscale.com>
2015-03-17 15:27:25 -0600

    correctly setup region from env:DATACENTER variable

--- recipes/ip_assign.rb
+++ recipes/ip_assign.rb
@@ -8,11 +8,14 @@ include_recipe 'rsc_google_cloud::default'
 # assumes the static IP is already created in the google compute console
 # supply the name of the static ip in the node["rsc_google_cloud"]["ip"]["name"] 
 # attribute
+datacenter =node[:rsc_google_cloud][:region].split('/').last.split('-')
+datacenter.delete(datacenter.last) #remove last part of the zone to get the region
+region = datacenter.join('-') #join array to get region
 
 gce_ip node["rsc_google_cloud"]["ip"]["name"] do
   name node["rsc_google_cloud"]["ip"]["name"]
   server node[:rsc_google_cloud][:instance_id].split('/').last
-  region node[:rsc_google_cloud][:region].split('/').last
+  region region
   project_id node["rsc_google_cloud"]["project_id"]
   client_email node["rsc_google_cloud"]["client_email"]
   json_key node["rsc_google_cloud"]["json_key"] 
