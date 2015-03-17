b582aec06e0baa9be159dbbee7f6fba99801878a
Curt Wilhelm <curt@rightscale.com>
2015-03-16 18:16:49 -0600

    fix attribute values.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,7 @@ require '/var/spool/cloud/meta-data.rb'
 end
 
 default[:rsc_google_cloud][:project_id]=nil
-default[:rsc_google_cloud][:instance_id]=nil
-default[:rsc_google_cloud][:region]=nil
+default[:rsc_google_cloud][:instance_id]=node[:rsc_google_cloud][:instance_id].split('/')[1]
+default[:rsc_google_cloud][:region]=node[:rsc_google_cloud][:region].split('/')[1]
 default[:rsc_google_cloud][:json_key]=nil
 default[:rsc_google_cloud][:ip][:name]=nil
-
-default["rsc_google_cloud"]["json_key"]= ""
